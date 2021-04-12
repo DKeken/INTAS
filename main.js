1580e1b3b5e061f22c9223e85a91848a0fc7cad4
WhiteDON <Thedon2200@gmail.com>
2021-04-12 05:32:31 +0300

    Исправления

--- main.js
+++ main.js
@@ -1,39 +1,26 @@
-(function ($) {
-    'use strict';
-
-    var form = $('.contact__form'),
-        message = $('.contact__msg'),
-        form_data;
-
-    // Success function
-    function done_func(response) {
-        message.fadeIn()
-        message.html(response);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 5000);
-        
-        form.find('input:not([type="submit"]), textarea').val('');
-    }
-
-    // fail function
-    function fail_func(data) {
-        message.fadeIn()
-        message.html(data.responseText);
-        setTimeout(function () {
-            message.fadeOut(5000);
-        }, 5000);
-    }
-    
-    form.submit(function (e) {
-        e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
-        })
-        .done(done_func)
-        .fail(fail_func);
+$("#contactForm").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+function submitForm(){
+    // Initiate Variables With Form Content
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+ 
+    $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message + "&phone=" + phone,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            }
+        }
     });
-})(jQuery);
+}
+function formSuccess(){
+    $( "#msgSubmit" ).removeClass( "d-none" );
+}
